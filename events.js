573806a42a4c05797de4aaaf00d17b604cc94802
Бойко Боев <boykopetevboev@gmail.com>
2020-04-17 21:57:10 +0300

    Create events.js

---
+++ events.js
@@ -0,0 +1,114 @@
+import { showUserData, updateProduct } from './database/requesterDB.js';
+import { register, login, logout } from './loginRegister/loginRegister.js';
+import { productForm, changeProduct } from './products/product.js';
+import { loadHomеPageInfo } from './homePage/homePage.js';
+
+const templatesPaths = {
+    header: './common/header.hbs',
+    footer: './common/footer.hbs',
+    menu: './homePage/menu.hbs',
+    userInfo: './common/userInfo.hbs'
+};
+function saveUserInfo(res) {
+    sessionStorage.setItem('email', res._id);
+    sessionStorage.setItem('code', res._rev);
+    sessionStorage.setItem('firstName', res.firstName);
+    sessionStorage.setItem('lastName', res.lastName);
+    sessionStorage.setItem('phone', res.phone);
+    sessionStorage.setItem('address', res.address);
+    if (res._id == 'admin') {
+        sessionStorage.setItem('admin', true)
+    }
+}
+function getUserInfo(ctx) {
+    ctx.email = sessionStorage.getItem('email');
+    ctx.code = sessionStorage.getItem('code');
+    ctx.firstName = sessionStorage.getItem('firstName')
+    ctx.lastName = sessionStorage.getItem('lastName')
+    ctx.phone = sessionStorage.getItem('phone')
+    ctx.address = sessionStorage.getItem('address')
+    ctx.admin = sessionStorage.getItem('admin')
+}
+(() => {
+    const app = Sammy('#eventsHolder', function () {
+        this.use('Handlebars', 'hbs');
+
+        this.get('/index.html', function (ctx) {
+            loadPage(ctx, './homePage/homePage.hbs');
+        })
+        this.get('#/home', function (ctx) {
+            loadHomеPage(ctx);
+        })
+        this.post('/register', function (ctx) {
+            const success = register(ctx)
+            if (success) {
+                success
+                    .then(res => {
+                        if(res){
+                            saveUserInfo(res);
+                            ctx.redirect('#/home');
+                        }
+                    })
+                    .catch(err => console.log(err));
+            }
+        })
+        this.get('/logout', function (ctx) {
+            logout(ctx);
+            ctx.redirect('#/home');
+        })
+        this.post('/login', function (ctx) {
+            const success = login(ctx);
+            if (success) {
+                success
+                    .then(res => {
+                        if (res) {
+                            saveUserInfo(res);
+                            ctx.redirect('#/home');
+                        }
+                    })
+                    .catch(err => console.log(err))
+            }
+        })
+        this.get('/login', function (ctx) {
+            loadPage(ctx, './LoginRegister/login.hbs');
+        })
+        this.get('/register', function (ctx) {
+            loadPage(ctx, './LoginRegister/register.hbs');
+        })
+        this.get('#/addProduct', function (ctx) {
+            loadPage(ctx, './products/addProductForm.hbs');
+        })
+        this.post('#/addProduct', productForm)
+        this.get('#/database', function (ctx) {
+            loadPage(ctx, './database/databaseInfo.hbs');
+        })
+        this.get('#/home/:id', function (ctx){
+            changeProduct(ctx)
+                .then(ctx => {
+                    loadPage(ctx, './products/changeproductInfo.hbs')
+                })
+                .catch(err => console.log(err))
+        })
+        this.post('#/changeProductInfo', function (ctx){
+            updateProduct(ctx.params._id, ctx.params)
+                .then(res => ctx.redirect('#/home'))
+                .catch(err => console.log(err))
+        })
+    })
+    app.run();
+})()
+function loadHomеPage(ctx){
+    getUserInfo(ctx)
+    loadHomеPageInfo(ctx)
+    .then(ctx => {
+        loadPage(ctx, './homePage/homePage.hbs');
+    })
+    .catch(err => console.log(err));
+}
+function loadPage(ctx, path) {
+    getUserInfo(ctx);
+    ctx.loadPartials(templatesPaths)
+        .partial(path)
+};
+
+export { loadPage }
