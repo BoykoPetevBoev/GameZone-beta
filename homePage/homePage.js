c0f18a60af0c21519dc12a9f35b31416a355cf48
Бойко Боев <boykopetevboev@gmail.com>
2020-04-23 20:08:06 +0300

    Update homePage.js

--- homePage/homePage.js
+++ homePage/homePage.js
@@ -1,12 +1,26 @@
 import { getProductData } from '../database/requesterDB.js'
 
-function loadHomеPageInfo(ctx){
-   return getProductData()
+function getProductsData(ctx) {
+    return getProductData()
         .then(data => {
-            ctx.data = data.rows.slice(0);
-            return ctx;
+            return data;
         })
         .catch(err => console.log(err))
-}   
+}
+function loadProductCategory(ctx) {
+    const pathCategory = {
+        '/mouse': 'mouse',
+        '/keyboard': 'keyboard',
+        '/headset': 'headset',
+        '/mousepad': 'mousepad',
+        '/accessoaries': 'accesoaries'
+    }
+    return getProductData(ctx)
+        .then(data => {
+            const category = data.rows.filter(product => product.doc.category == pathCategory[ctx.path])
+            return category
+        })
+        .catch(err => console.log(err))
+}
 
-export { loadHomеPageInfo };
+export { getProductsData, loadProductCategory  };
