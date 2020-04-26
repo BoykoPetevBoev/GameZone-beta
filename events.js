b2fb3d1cfca9c0580e14652389159ec8d2ecb6e8
Бойко Боев <boykopetevboev@gmail.com>
2020-04-26 21:18:11 +0300

    Update events.js

--- events.js
+++ events.js
@@ -1,14 +1,26 @@
-import { showUserData, updateProduct } from './database/requesterDB.js';
+
+import { showUserData, updateProduct, getProductData } from './database/requesterDB.js';
 import { register, login, logout } from './loginRegister/loginRegister.js';
-import { productForm, changeProduct, loadProductPage } from './products/product.js';
-import { getProductsData, loadProductCategory } from './homePage/homePage.js';
+import { productForm, loadChangeProductPage, loadProductPage } from './products/product.js';
+import { loadPageCategory, loadHomеPage } from './homePage/homePage.js';
 // import { pushNotifications } from "./pushNotifications/pushNotifications.js"
 
-const templatesPaths = {
+const commonPaths = {
     header: './common/header.hbs',
     footer: './common/footer.hbs',
     menu: './homePage/menu.hbs',
     userInfo: './common/userInfo.hbs'
+};
+const templatesPaths = {
+    '/#/login': './LoginRegister/login.hbs',
+    '/#/register': './LoginRegister/register.hbs',
+    '/#/productForm': './products/addProductForm.hbs',
+    '/#/database': './database/databaseInfo.hbs',
+    '/#/home/mouse': './homePage/homePage.hbs',
+    '/#/home/keyboard': './homePage/homePage.hbs',
+    '/#/home/headset': './homePage/homePage.hbs',
+    '/#/home/mousepad': './homePage/homePage.hbs',
+    '/#/home/accessoaries': './homePage/homePage.hbs'
 };
 function saveUserInfo(res) {
     sessionStorage.setItem('email', res._id);
@@ -18,139 +30,44 @@ function saveUserInfo(res) {
     sessionStorage.setItem('phone', res.phone);
     sessionStorage.setItem('address', res.address);
     if (res._id == 'admin') {
-        sessionStorage.setItem('admin', true)
+        sessionStorage.setItem('admin', true);
     }
 }
 function getUserInfoFrom(ctx) {
     ctx.email = sessionStorage.getItem('email');
     ctx.code = sessionStorage.getItem('code');
-    ctx.firstName = sessionStorage.getItem('firstName')
-    ctx.lastName = sessionStorage.getItem('lastName')
-    ctx.phone = sessionStorage.getItem('phone')
-    ctx.address = sessionStorage.getItem('address')
-    ctx.admin = sessionStorage.getItem('admin')
+    ctx.firstName = sessionStorage.getItem('firstName');
+    ctx.lastName = sessionStorage.getItem('lastName');
+    ctx.phone = sessionStorage.getItem('phone');
+    ctx.address = sessionStorage.getItem('address');
+    ctx.admin = sessionStorage.getItem('admin');
 }
 (() => {
     const app = Sammy('#eventsHolder', function () {
         this.use('Handlebars', 'hbs');
 
-        this.get('index.html', function (ctx) {
-            ctx.redirect('/');
-        });
-        this.get('#/home', function (ctx) {
-            ctx.redirect('/');
-        });
-        this.get('/', function (ctx) {
-            loadHomеPage(ctx);
-        });
-        this.post('/register', function (ctx) {
-            const success = register(ctx)
-            if (success) {
-                success
-                    .then(res => {
-                        if (res) {
-                            saveUserInfo(res);
-                            ctx.redirect('#/home');
-                        }
-                    })
-                    .catch(err => console.log(err));
-            }
-        });
-        this.get('/logout', function (ctx) {
-            logout(ctx);
-            ctx.redirect('#/home');
-        });
-        this.post('/login', function (ctx) {
-            const success = login(ctx);
-            if (success) {
-                success
-                    .then(res => {
-                        if (res) {
-                            saveUserInfo(res);
-                            ctx.redirect('#/home');
-                        }
-                    })
-                    .catch(err => console.log(err))
-            }
-        });
-        this.get('/login', function (ctx) {
-            loadPage(ctx, './LoginRegister/login.hbs');
-        });
-        this.get('/register', function (ctx) {
-            loadPage(ctx, './LoginRegister/register.hbs');
-        });
-        this.get('#/addProduct', function (ctx) {
-            loadPage(ctx, './products/addProductForm.hbs');
-        });
-        this.post('#/addProduct', productForm)
-        this.get('#/database', function (ctx) {
-            loadPage(ctx, './database/databaseInfo.hbs');
-        });
-        this.get('/mouse', function(ctx){
-            getUserInfoFrom(ctx)
-            loadProductCategory(ctx)
-                .then(data => {
-                    ctx.data = data.slice(0);
-                    loadPage(ctx, './homePage/homePage.hbs');
-                })
-                .catch(err => console.log(err));
-        });
-        this.get('/keyboard', function(ctx){
-            getUserInfoFrom(ctx)
-            loadProductCategory(ctx)
-                .then(data => {
-                    ctx.data = data.slice(0);
-                    loadPage(ctx, './homePage/homePage.hbs');
-                })
-                .catch(err => console.log(err));
-        });
-        this.get('/headset', function(ctx){
-            getUserInfoFrom(ctx)
-            loadProductCategory(ctx)
-                .then(data => {
-                    ctx.data = data.slice(0);
-                    loadPage(ctx, './homePage/homePage.hbs');
-                })
-                .catch(err => console.log(err));
-        });
-        this.get('/mousepad', function(ctx){
-            getUserInfoFrom(ctx)
-            loadProductCategory(ctx)
-                .then(data => {
-                    ctx.data = data.slice(0);
-                    loadPage(ctx, './homePage/homePage.hbs');
-                })
-                .catch(err => console.log(err));
-        });
-        this.get('/accessoaries', function(ctx){
-            getUserInfoFrom(ctx)
-            loadProductCategory(ctx)
-                .then(data => {
-                    ctx.data = data.slice(0);
-                    loadPage(ctx, './homePage/homePage.hbs');
-                })
-                .catch(err => console.log(err));
-        });
-        this.get('/:id', function(ctx) {
-            loadProductPage(ctx)
-                .then(ctx => {
-                    console.log(ctx)
-                    loadPage(ctx, './products/productPage.hbs')
-                })
-                .catch(err => console.log(err))
-        } )
-        this.get('/keyboard/:id', loadProductPage)
-        this.get('/headset/:id', loadProductPage)
-        this.get('/mousepad/:id', loadProductPage)
-        this.get('/accessoaries/:id', loadProductPage)
-        
-        this.get('/:id', function (ctx) {
-            changeProduct(ctx)
-                .then(ctx => {
-                    loadPage(ctx, './homePage/homePage.hbs')
-                })
-                .catch(err => console.log(err))
-        });
+        this.get('index.html', function (ctx) { ctx.redirect('/') });
+        this.get('/', function (ctx) { ctx.redirect('#/home') });
+        this.get('#/home', loadHomеPage);
+        this.get('#/register', loadPage);
+        this.post('#/register', register);
+        this.get('#/login', loadPage);
+        this.post('#/login', login);
+        this.get('#/logout', logout);
+        this.get('#/productForm', loadPage);
+        this.post('#/productForm', productForm);
+        this.get('#/database', loadPage);
+        this.get('#/home/mouse', loadPageCategory);
+        this.get('#/home/keyboard', loadPageCategory);
+        this.get('#/home/headset', loadPageCategory);
+        this.get('#/home/mousepad', loadPageCategory);
+        this.get('#/home/accessoaries', loadPageCategory);
+        this.get('#/home/mouse/:id', loadProductPage);
+        this.get('#/home/keyboard/:id', loadProductPage);
+        this.get('#/home/headset/:id', loadProductPage);
+        this.get('#/home/mousepad/:id', loadProductPage);
+        this.get('#/home/accessoaries/:id', loadProductPage);
+        this.get('#/productForm/:id', loadChangeProductPage);
         this.post('#/changeProductInfo', function (ctx) {
             updateProduct(ctx.params._id, ctx.params)
                 .then(res => ctx.redirect('#/home'))
@@ -160,22 +77,16 @@ function getUserInfoFrom(ctx) {
     app.run();
 })()
 
-
-
-function loadHomеPage(ctx) {
-    getUserInfoFrom(ctx)
-    getProductsData(ctx)
-        .then(data => {
-            ctx.data = data.rows.slice(0);
-            loadPage(ctx, './homePage/homePage.hbs');
-        })
-        .catch(err => console.log(err));
-}
-function loadPage(ctx, path) {
+function loadPage(ctx, p) {
+    let path = '';
+    templatesPaths.hasOwnProperty(ctx.path)
+        ? path = templatesPaths[ctx.path]
+        : path = p;
     getUserInfoFrom(ctx);
-    ctx.loadPartials(templatesPaths)
+    ctx.loadPartials(commonPaths)
         .partial(path);
 };
 
 
-export { loadPage }
+export { loadPage, saveUserInfo, getUserInfoFrom }
+
